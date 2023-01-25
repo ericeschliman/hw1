4fa40bf2536015c11e73dca60d0542abbff8a85c
ericeschliman <eric.eschliman@kellogg.northwestern.edu>
2023-01-25 20:28:35 +0000

    first iteration of hw1

--- kmdb.sql
+++ kmdb.sql
@@ -102,12 +102,59 @@
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
 
+DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS characters;
+
 -- Create new tables, according to your domain model
 -- TODO!
+
+CREATE TABLE studios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    studio_name TEXT
+);
+
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    movie_name TEXT,
+    year_released INTEGER,  
+    rating TEXT,
+    studio_id  INTEGER,
+    actor_id INTEGER,
+    character_id INTEGER
+);
+
+CREATE TABLE actors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    actor_name TEXT,
+    character_id INTEGER
+);
+
+CREATE TABLE characters (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    character_name TEXT
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
+
+INSERT INTO studios (
+    "studio_name"
+) VALUES (
+    "Warner Bros."
+);
+
+INSERT INTO movies (
+    "movie_name",
+    "year_released",
+    "rating"
+) VALUES (
+    "Batman Begins",
+    "2005",
+    "PG-13"
+);
 
 -- Prints a header for the movies output
 .print "Movies"
@@ -116,6 +163,9 @@
 
 -- The SQL statement for the movies output
 -- TODO!
+
+SELECT * FROM movies;
+SELECT * FROM studios;
 
 -- Prints a header for the cast output
 .print ""
@@ -126,3 +176,4 @@
 
 -- The SQL statement for the cast output
 -- TODO!
+
